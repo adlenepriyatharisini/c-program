2e52db830663ba3bcf6bab7cfdca3582983e1ee9
adlenepriyatharisini <48051984+adlenepriyatharisini@users.noreply.github.com>
2019-03-01 16:14:31 +0530

    Create begset1_3.c

---
+++ begset1_3.c
@@ -0,0 +1,11 @@
+char a;
+printf("enter the character");
+scanf("%c",&a);
+if(a=='a'|| a=='e'|| a=='i'|| a=='o'|| a=='u'|| a=='A'|| a=='E'|| a=='I'|| a=='O'|| a=='U');
+{
+printf("vowels')"
+}
+else
+{
+printf("consonent");
+}
