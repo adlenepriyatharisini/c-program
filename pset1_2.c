ca8a38a0e38604f9a582c202467e4baf0b4ec9db
adlenepriyatharisini <48051984+adlenepriyatharisini@users.noreply.github.com>
2019-03-15 15:38:27 +0530

    Create pset1_2.c

---
+++ pset1_2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main();
+{
+int N,i,fact=1;
+printf("N value");
+scanf("%d",&N);
+if(n<=20)
+{
+for(i=1;i<=n;i++)
+{
+fact=fact*i;
+}
+printf("%d",fact);
+}
+else
+printf(" ");
+return 0;
+}
